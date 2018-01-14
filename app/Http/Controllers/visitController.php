89b62b5969eaae7bb2f6b9298f71b6c8335b614c
mohamad <sarayi.cactux@gmail.com>
2018-01-14 18:03:25 +0330

    C

--- app/Http/Controllers/visitController.php
+++ app/Http/Controllers/visitController.php
@@ -12,6 +12,7 @@ use App\Helpers\Jdate;
 use App\Models\pro_city;
 use Session;
 use Verta;
+use Cookie;
 require_once('nusoap.php');
 use nusoap_client;
 
@@ -19,8 +20,10 @@ class visitController extends Controller
 {
     public function index(Request $request){
        // Session::forget('customer');
-
-
+        $v = verta();
+       // setcookie('referer', $_SERVER['HTTP_REFERER'], time() + (86400 * 30), "/");
+      //  unset($_COOKIE['referer']);
+      // echo $_COOKIE['referer'];
         if ( Session::has('customer') ){
             $date = Jdate::medate();
             $pros = pro_city::where('pro_id',0)->orderBy('id','ASC')->get();
@@ -30,48 +33,69 @@ class visitController extends Controller
             return view('layouts.customer', array('date'=>Jdate::fn($date['date4']),'pros'=>$pros,'cities'=>$cities,'order'=>$order,'sales'=>$sales));
         }
         else {
-            $v = verta();
+
+
             session([
                 'url_id'=>'1',
                 'url_ref'=>'anySite'
             ]);
-            if (isset($_SERVER['HTTP_REFERER'])){
+            if (isset($_COOKIE['referer'])){
 
-                $url = url::where('url','like',$_SERVER['HTTP_REFERER'])->get();
+                $url = url::where('url','like',$_COOKIE['referer'])->get();
                 // var_dump($url);
                 if (count($url) > 0){
+                    setcookie('referer', $_COOKIE['referer'], time() + (86400 * 30), "/");
                     $url = url::find($url[0]->id);
                     $url->visits()->create([
-                        'url_ref'   =>$_SERVER['HTTP_REFERER'],
+                        'url_ref'   =>$_COOKIE['referer'],
                         'visit_time'=>$v->hour.':'.$v->minute.':'.$v->second,
                         'visit_date'=>$v->year.'/'.$v->month.'/'.$v->day
-                    ]);
-
-                }
-                else {
-                    $url = url::find(1);
-                    $url->visits()->create([
-                        'url_ref'   =>$_SERVER['HTTP_REFERER'],
-                        'visit_time'=>$v->hour.':'.$v->minute.':'.$v->second,
-                        'visit_date'=>$v->year.'/'.$v->month.'/'.$v->day
-                    ]);
-                    session([
-                        'url_id'=>$url->id,
-                        'url_ref'=>$_SERVER['HTTP_REFERER']
                     ]);
 
                 }
             }
             else {
-                $url = url::find(1);
-                $url->visits()->create([
-                    'url_ref'   =>'anySite',
-                    'visit_time'=>$v->hour.':'.$v->minute.':'.$v->second,
-                    'visit_date'=>$v->year.'/'.$v->month.'/'.$v->day,
-                ]);
+                if (isset($_SERVER['HTTP_REFERER'])){
+
+                    $url = url::where('url','like',$_SERVER['HTTP_REFERER'])->get();
+                    // var_dump($url);
+                    if (count($url) > 0){
+
+                        setcookie('referer', $_SERVER['HTTP_REFERER'], time() + (86400 * 30), "/");
+                        $url = url::find($url[0]->id);
+                        $url->visits()->create([
+                            'url_ref'   =>$_SERVER['HTTP_REFERER'],
+                            'visit_time'=>$v->hour.':'.$v->minute.':'.$v->second,
+                            'visit_date'=>$v->year.'/'.$v->month.'/'.$v->day
+                        ]);
+
+                    }
+                    else {
+                        $url = url::find(1);
+                        $url->visits()->create([
+                            'url_ref'   =>$_SERVER['HTTP_REFERER'],
+                            'visit_time'=>$v->hour.':'.$v->minute.':'.$v->second,
+                            'visit_date'=>$v->year.'/'.$v->month.'/'.$v->day
+                        ]);
+                        session([
+                            'url_id'=>$url->id,
+                            'url_ref'=>$_SERVER['HTTP_REFERER']
+                        ]);
+
+                    }
+                }
+                else {
+                    $url = url::find(1);
+                    $url->visits()->create([
+                        'url_ref'   =>'anySite',
+                        'visit_time'=>$v->hour.':'.$v->minute.':'.$v->second,
+                        'visit_date'=>$v->year.'/'.$v->month.'/'.$v->day,
+                    ]);
 
 
+                }
             }
+
             $pros = pro_city::where('pro_id',0)->orderBy('id','ASC')->get();
             $cities = pro_city::where('pro_id',1)->orderBy('id','ASC')->get();
             $date = Jdate::medate();
@@ -82,6 +106,6 @@ class visitController extends Controller
     }
     public function myUrl(){
 
-        if (isset($_SERVER['HTTP_REFERER'])) echo "You Refere URL Is : ".$_SERVER['HTTP_REFERER'];
+        if (isset($_SERVER['HTTP_REFERER'])) echo "You Referer URL Is : ".$_SERVER['HTTP_REFERER'];
     }
 }
